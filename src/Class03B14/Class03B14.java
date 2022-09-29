b0f116d1502235ee842915b3184d142804ea9221
ylflores <floresyelyzaveta@gmail.com>
2022-09-29 18:37:00 -0500

    second commit

--- src/Class03B14/Class03B14.java
+++ src/Class03B14/Class03B14.java
@@ -5,6 +5,7 @@ public class Class03B14 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hey this is git class");
+System.out.println("this is a change");
 	}
 
 }
